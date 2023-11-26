c03756ce62c5f7693b8073bbadc618155cbae3e3
Sai Surya Teja <saisuryateja3@gmail.com>
2023-11-27 00:17:05 +0530

    Added Hero page

---
+++ components/Hero.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { RiArrowDownSLine, RiDownload2Fill } from "react-icons/ri";
+import { Send } from "lucide-react";
+const Hero = () => {
+  return (
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-pink-100">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-x-6">
+          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
+              Web Developer
+            </div>
+            <h1 className="h1 mb-4">Hello, My name is Sai Surya Teja</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              Brief description with insights tnto myself, my vocational
+              journey, and what I engage in professionally.
+            </p>
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3  xl-mx:0 mb-12 ">
+              <Link href={"/contact"}>
+                <Button className="gap-x-2">
+                  Contact me <Send size={18} />
+                </Button>
+              </Link>
+              <Link href={"/contact"}>
+                <Button className="gap-x-2" variant={"secondary"}>
+                  Download CV <RiDownload2Fill size={18} />
+                </Button>
+              </Link>
+            </div>
+          </div>
+
+          <div className="hidden xl:flex  relative">Image</div>
+        </div>
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
