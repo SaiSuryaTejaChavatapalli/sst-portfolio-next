9bc2d0d339d9852740c7d4d0761acb2763dd7c51
Sai Surya Teja <saisuryateja3@gmail.com>
2023-11-27 23:51:27 +0530

    Footer Added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,59 @@
+"use client";
+import React, { useState } from "react";
+import { Tabs } from "@/components/ui/tabs";
+
+const projectData = [
+  {
+    image: "/work/1.png",
+    category: "react js",
+    name: "Nexa Website",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "",
+    github: "",
+  },
+  {
+    image: "/work/2.png",
+    category: "next js",
+    name: "Nexa Website",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "",
+    github: "",
+  },
+  {
+    image: "/work/3.png",
+    category: "next js",
+    name: "Nexa Website",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "",
+    github: "",
+  },
+  {
+    image: "/work/4.png",
+    category: "full stack",
+    name: "Nexa Website",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "",
+    github: "",
+  },
+  {
+    image: "/work/2.png",
+    category: "vannila js",
+    name: "Nexa Website",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "",
+    github: "",
+  },
+  {
+    image: "/work/1.png",
+    category: "react js",
+    name: "Nexa Website",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    link: "",
+    github: "",
+  },
+];
 const ProjectsPage = () => {
-  return <div>ProjectsPage</div>;
+  return <section className="container min-h-screen pt-12"></section>;
 };
 
 export default ProjectsPage;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,19 @@
+import Socials from "./Socials";
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-black py-12">
+      <div className="flex flex-col items-center justify-between">
+        {/* socials */}
+        <Socials
+          containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4"
+          iconStyles="text-primary text-[20px] dark:text-white/70 hover:text-white dark:hover:text-primary transition-all"
+        />
+        <div className="text-muted-foreground">
+          Copyright &copy; Sai Surya Teja Chavatapalli. ALl rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
