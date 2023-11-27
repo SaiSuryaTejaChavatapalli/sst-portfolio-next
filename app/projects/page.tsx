a83aa5ca7754fbe5c40ed05ed011646a941d0b51
Sai Surya Teja <saisuryateja3@gmail.com>
2023-11-28 00:37:57 +0530

    Added MyProjects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import { Tabs } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import ProjectCard from "@/components/ProjectCard";
 
 const projectData = [
   {
@@ -52,8 +53,58 @@ const projectData = [
     github: "",
   },
 ];
+
+const uniqueCategories = [
+  "all projects",
+  ...new Set(projectData.map((item) => item.category)),
+];
+
 const ProjectsPage = () => {
-  return <section className="container min-h-screen pt-12"></section>;
+  const [categories, setCategories] = useState(uniqueCategories);
+  const [category, setCategory] = useState("all projects");
+
+  const filteredProjects = projectData.filter((project) => {
+    return category === "all projects"
+      ? project
+      : project.category === category;
+  });
+  console.log("Filtered Projects", filteredProjects);
+  return (
+    <section className="container min-h-screen pt-12">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto text-primary">
+          My Projects
+        </h2>
+        {/* Tabs */}
+        <Tabs defaultValue={"all projects"} className="mb-24 xl:mb-48">
+          <TabsList className="w-full grid h-full md:grid-cols-5 lg:max-w-[640px] mb-12 mx-auto md:border dark:border-none">
+            {categories.map((cateory, index) => {
+              return (
+                <TabsTrigger
+                  onClick={() => setCategory(cateory)}
+                  value={cateory}
+                  key={index}
+                  className="capitalize w-[162px] md:w-auto"
+                >
+                  {cateory}
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+          {/* Tabs content */}
+          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {filteredProjects.map((project, index) => {
+              return (
+                <TabsContent value={category} key={index}>
+                  <ProjectCard project={project} />
+                </TabsContent>
+              );
+            })}
+          </div>
+        </Tabs>
+      </div>
+    </section>
+  );
 };
 
 export default ProjectsPage;
