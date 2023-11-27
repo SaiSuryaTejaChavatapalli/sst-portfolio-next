1977fb60aec2ffde3dfb8f53299c965851979f93
Sai Surya Teja <saisuryateja3@gmail.com>
2023-11-27 15:59:12 +0530

    Added personal info and education in about

--- components/About.tsx
+++ components/About.tsx
@@ -64,7 +64,7 @@ const qualificationData = [
     data: [
       {
         company: "Persistent Systems",
-        qualification: "Software Engineer",
+        role: "Software Engineer",
         years: "2022-present",
       },
     ],
@@ -107,7 +107,7 @@ const skillsData = [
   },
 ];
 const About = () => {
-  const getData = (arr: { title: string }[], title: string) => {
+  const getData = (arr, title) => {
     return arr.find((item) => item.title === title);
   };
   return (
@@ -165,12 +165,96 @@ const About = () => {
                       })}
                     </div>
                     {/* languages */}
-                    <div className="text-primary">Language Skill</div>
-                    <div className="border-b"></div>
-                    <div>English, Telugu</div>
+                    <div className="flex flex-col gap-y-2">
+                      <div className="text-primary">Language Skill</div>
+                      <div className="border-b"></div>
+                      <div>English, Telugu</div>
+                    </div>
                   </div>
                 </TabsContent>
-                <TabsContent value="qualifications">Qualifications</TabsContent>
+                <TabsContent value="qualifications">
+                  <div>
+                    <h3 className="h3 mb-8 text-center xl:text-left">
+                      My Awesome Journey
+                    </h3>
+                    {/* experience and education wrapper*/}
+                    <div className="grid md:grid-cols-2 gap-y-8">
+                      {/* experience */}
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <Briefcase />
+                          <div className="capitalize font-medium">
+                            {getData(qualificationData, "experience").title}
+                          </div>
+                        </div>
+                        {/* list */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "experience").data.map(
+                            (item, index) => {
+                              const { company, role, years } = item;
+                              return (
+                                <div
+                                  key={index}
+                                  className="flex gap-x-8 group "
+                                >
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {company}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {role}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      </div>
+                      {/* education */}
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <GraduationCap size={28} />
+                          <div className="capitalize font-medium">
+                            {getData(qualificationData, "education").title}
+                          </div>
+                        </div>
+                        {/* list */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "education").data.map(
+                            (item, index) => {
+                              const { university, qualification, years } = item;
+                              return (
+                                <div key={index} className="flex gap-x-8 group">
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {university}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {qualification}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </TabsContent>
                 <TabsContent value="skills">Skills Info</TabsContent>
               </div>
             </Tabs>
